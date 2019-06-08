18b9422390abbaa18b96c1209dfccce68f423681
JGabrielVargasR <45431548+JGabrielVargasR@users.noreply.github.com>
2019-06-07 20:47:26 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -2,10 +2,12 @@ var mymap = L.map('map').setView([4.8119, -74.2341], 11);
 
 var OpenStreetMap = L.tileLayer.provider('OpenStreetMap').addTo(mymap);
 var GoogleStreetMap = L.tileLayer.provider('GoogleMaps').addTo(mymap);
+var GoogleTrafficMap = L.tileLayer.provider('GoogleTraffic').addTo(mymap);
 
 var baseMaps = {
 	"OpenStreetMap": OpenStreetMap,
-	"Google Maps": GoogleStreetMap
+	"Google Maps": GoogleStreetMap,
+	"Google Traffic": GoogleTrafficMap
 };
 var overlayMaps = {
 };
