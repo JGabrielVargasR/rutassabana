ae41bc75746f60cea4ade8f23065f1310ad93331
JGabrielVargasR <45431548+JGabrielVargasR@users.noreply.github.com>
2019-06-09 17:12:26 -0500

    updated

--- js/location_map.js
+++ js/location_map.js
@@ -10,9 +10,9 @@ function getLocation(){
 				
 		map.locate({setView: true, maxZoom: 18});
 		function onLocationFound(e) {
-			var radius = e.accuracy*5;
-				L.marker(e.latlng).addTo(map);
-			L.circle(e.latlng, radius).addTo(map);
+			var radius = e.accuracy/2;
+			var marker = L.marker(e.latlng).addTo(map);
+			var circle = L.circle(e.latlng, radius).addTo(map);
 			}
 		map.on('locationfound', onLocationFound);
 	}else {
