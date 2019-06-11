fd681683f38c5b0051e721866a51ff5cc16ec40c
JGabrielVargasR <45431548+JGabrielVargasR@users.noreply.github.com>
2019-06-11 18:15:41 -0500

    Update

--- js/map.js
+++ js/map.js
@@ -13,3 +13,5 @@ var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(mymap);
 var layers = {};
 var db_path = '../sqlite/prueba.sqlite';
 var municipios = execute_query(db_path,"select AsGeoJSON(Geometry), nombre from p_municipio",mymap,'Municipios',['#query-output',false],mpio_style);
+
+var ruta = execute_query(db_path,"select AsGeoJSON(geometry) from vias_net where NodeFrom = 327005 and NodeTo = 380685",mymap,'Prueba Ruta',['#query-output',false],ruta_style);
