ee2e291b110436947ee50c5de7a03470752aa152
JGabrielVargasR <45431548+JGabrielVargasR@users.noreply.github.com>
2019-06-11 20:59:34 -0500

    update

--- js/location_map.js
+++ js/location_map.js
@@ -13,12 +13,14 @@ function getLocation(){
 			var marker = L.marker(e.latlng).addTo(map);
 			}
 		map.on('locationfound', onLocationFound);
+		
+		
 	}else {
 		alert("Geolocation API is not supported in your browser. :(");
 	}
 }
 
 function loadPoint(id, latitude, longitude){
-	var ruta = execute_query(db_path,"select asGeoJSON(geom), id_parada, pt_parada.nombre as nom_parada, direccion , p_municipio.nombre as nom_mpio from pt_parada, p_municipio where id_mpio = mpio_divip and id_parada = '"+id+"'",map,'Parada',['#query-output',false],markerIcon);
+	var ruta = execute_query(db_path,"select asGeoJSON(geom), id_parada, pt_parada.nombre as nom_parada, direccion , p_municipio.nombre as nom_mpio from pt_parada, p_municipio where id_mpio = mpio_divip and id_parada = '"+id+"'",map,'Parada',['#query-output',false],stopIcon);
 	map.setView([latitude, longitude], 18);
 	}
--- js/styles.js
+++ js/styles.js
@@ -33,6 +33,17 @@ var markerIcon = L.icon({
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
+var stopIcon = L.icon({
+    iconUrl: '../css/images/stop-icon.png',
+    shadowUrl: '../css/images/marker-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
 var ruta_style = function (feature) {
       if(feature.properties.origen == 'MADRID'){
         return {color: 'red', weight: 4};
